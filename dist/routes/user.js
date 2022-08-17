23253635288ecb50669ea4c6ac8c031274d1a22f
AlejandroAlbites <81944337+AlejandroAlbites@users.noreply.github.com>
2022-08-16 22:58:39 -0500

    Delete dist directory

--- dist/routes/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_1 = require("../controllers/user");
-const router = (0, express_1.Router)();
-router.route('/').post(user_1.registerUser);
-router.route('/').get(user_1.getUser);
-exports.default = router;
-//# sourceMappingURL=user.js.map
